59f8159754b4ca0eb08c727251dfd82526524820
vmtct <56582243+vmtct@users.noreply.github.com>
2026-08-26 09:20:50 +0700

    Load PianoHouse short polish styles

---
+++ app/pianohouse/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "./short-polish.css";
+
+export default function PianoHouseLayout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
