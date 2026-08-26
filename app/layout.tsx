cab09a5c0a3998e680b9456e9584e0915449cfc3
vmtct <56582243+vmtct@users.noreply.github.com>
2026-08-26 15:23:26 +0700

    style: load localization controls

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./homepage-mobile.css";
+import "./localization.css";
 import CmsHydrator from "./cms-hydrator";
 import { LocaleProvider } from "./localization";
 
